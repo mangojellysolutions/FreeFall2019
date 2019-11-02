f8604325634844dc579cee5997985684fefd9c70
mangojellysolutions <49659915+mangojellysolutions@users.noreply.github.com>
2019-11-02 16:13:17 +0000

    24th done, challenge completed

---
+++ 24/collectable.js
@@ -0,0 +1,55 @@
+const Collectable = (x, y) => {
+	const size = { w:10, h:10 }
+	const position  = { x:x, y:y };
+	const speed = 1;
+	let count =0;
+	let frame = 0;
+	let step = 1;
+	const draw = () => {
+		//ctx.drawImage(platformImg[idx],position.x,position.y);
+		//drawBounds();
+		ctx.save();
+		ctx.lineWidth = 2;
+		const blur = 5;
+		ctx.shadowColor = '#FFF'
+		ctx.shadowOffsetX = 0;
+		ctx.shadowOffsetY = 0;
+		ctx.shadowBlur = blur;
+		ctx.strokeStyle = 'white';
+		
+		ctx.beginPath();
+		ctx.ellipse(position.x+5, position.y+2, 1+frame, size.h, Math.PI *2, 0, 2 * Math.PI);
+		
+		ctx.stroke();
+		ctx.restore();
+		count++;
+		if (count > 5){
+			count = 0;
+			frame +=step;
+			if (frame > size.w || frame < 0){
+				step = -step;
+			}
+		}
+	}
+	const move = () => position.y -= speed;
+	const getPosition = () =>  position; 
+	const getBounds = () => { return {x: position.x, y: position.y+5, width: size.w, height: size.h} }
+	const drawBounds = () => {
+		const rect2 = getBounds();
+		ctx.fillRect(rect2.x,rect2.y,rect2.width, rect2.height);
+	}
+	const intersects = (playerBounds) => {
+		const rect1 = playerBounds;
+		const rect2 = getBounds();
+		if (rect1.x < rect2.x + rect2.width &&
+		   rect1.x + rect1.width > rect2.x &&
+		   rect1.y < rect2.y + rect2.height &&
+		   rect1.y + rect1.height > rect2.y) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	return { draw, move, intersects, getPosition }
+}
+
---
+++ 24/player.js
@@ -0,0 +1,139 @@
+const Player = (x ,y) => {
+	const size = {w: 32, h: 32};
+	const position  = { x:x, y:y };
+	
+	let speed = 0;
+	const defaultFallRate = 1.5;
+	const maxFallRate = 10;
+	let refreshCount = 0;
+	let frameIdx = 0;
+	let fallRate = defaultFallRate;
+	let state = 'static';
+	let distanceTravelled = 0;
+	let isFalling = false;
+	let direction = 'right';
+	const setState = (keyCode) => {
+		//if (isFalling){
+		//	state = 'falling';
+		//} else {
+			switch(keyCode){
+				case 37: state = 'left'; direction = state; break; //Left key
+				case 38: state = 'jump'; break;//Up key
+				case 39: state = 'right'; direction = state; break; //Right key
+				case 40: state = 'duck'; break; //Down key
+				default: state = 'static'; break;
+			}
+		//}
+	}
+	
+	const getBounds = () => { return {x: position.x, y: position.y-size.h+10, width: size.w, height: size.h} }
+	const getPosition = () => { return position; }
+	const getSize = () => { return size; }
+	const setPosition = (x, y) => {
+		position.x = x;
+		position.y = y;
+	}
+	
+	const animate = () => {
+		if (isFalling){
+			frameIdx = 1;
+			distanceTravelled = 0
+		} else {
+			switch(state){
+				case 'left':
+				case 'right':
+					distanceTravelled += Math.abs(speed);
+					if (distanceTravelled > 5 ){
+						frameIdx ++;
+						if (frameIdx > 3){
+							frameIdx = 0;
+						}
+						distanceTravelled = 0;
+					}
+				break;
+			}
+		}
+	}
+	
+	const draw = () => {
+		//should precalculate these images first
+		ctx.fillStyle = "red";
+		//drawBounds();
+		if (direction == 'left'){
+			ctx.save();
+			ctx.translate(position.x +size.w, position.y-size.h+10);
+			ctx.scale(-1, 1);
+			ctx.drawImage(playerFrames[frameIdx],0,0,size.w,size.h);
+			ctx.restore();
+		} else {
+			ctx.drawImage(playerFrames[frameIdx],position.x, position.y-size.h+10,size.w,size.h);
+		}
+		
+		ctx.fillStyle = "blue";
+	}
+	
+	const drawBounds = () => {
+		const rect1 = getBounds();
+		ctx.fillRect(rect1.x,rect1.y,rect1.width, rect1.height);
+	}
+	
+	const moveRight = () => speed += 0.2
+	const moveLeft = () => speed -= 0.2
+	
+	const skidd = () => {
+		if (Math.abs(speed)>0){
+			if (speed > 0){
+				speed -= 0.2;
+				if (speed < 0){
+					speed = 0;
+				}
+			} else
+			if (speed < 0){
+				speed += 0.2;
+				if (speed > 0){
+					speed = 0;
+				}
+			}
+		}
+	}
+	const wrap =() => {
+		if (position.x < 0)
+			position.x = canvas.width;
+		if (position.x > canvas.width)
+			position.x = 0;
+	}
+	
+	const move = () => {
+		switch(state){
+			case 'left': moveLeft(); break;
+			case 'right': moveRight();	break;
+			default: skidd();
+			
+		}
+		wrap();
+		position.x += speed;
+	}
+	
+	const reachedBottomOfScreen = () => {
+		if (position.y > canvas.height){
+			position.y = 0;
+			fallRate = defaultFallRate;
+		}
+	}
+	
+	const drop = (hasColliedWithPlatform) => {	
+		if (hasColliedWithPlatform){
+			fallRate = defaultFallRate;
+			isFalling = false;
+		} else {
+			isFalling = true;
+			if (fallRate < maxFallRate){
+				fallRate = fallRate * 1.01;
+			}
+			position.y += fallRate;
+			reachedBottomOfScreen();
+		}
+	}
+	
+	return { draw, setState, move, drop, getPosition, setPosition, animate, getSize, getBounds }
+}
